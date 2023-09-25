1f7cd9c94b583faee4ed37f4ebcde6715fc205a4
sunil_malani <sunilmalani456@gmail.com>
2023-09-25 11:45:33 +0530

    complete project commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Header from "@/components/Header";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
@@ -9,7 +9,10 @@ import ThemeContextProvider from "@/context/theme-context";
 import ThemeSwitch from "@/components/ThemeSwitch";
 import Script from "next/script";
 
-const inter = Inter({ subsets: ["latin"] });
+const acorn = localFont({
+  src: "../public/fonts/acorn.woff",
+  variable: "--font--acorn",
+});
 
 export const metadata: Metadata = {
   title: "Sunil Malani | Personal Portfolio",
@@ -24,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${acorn.variable} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,15 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        acorn: "var(--font--acorn)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      acorn: ["Acorn SemiBold"],
     },
   },
   plugins: [],
